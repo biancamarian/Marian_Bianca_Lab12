19e315cf8440aa1ac1260382c015a1307e30b2ee
Bianca Marian <bia.nico47@yahoo.com>
2021-01-06 11:16:16 +0200

    Add project files.

---
+++ Marian_Bianca_Lab12/Marian_Bianca_Lab12/Data/IRestService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Marian_Bianca_Lab12.Models;
+
+namespace Marian_Bianca_Lab12.Data
+{
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+
+    }
+}
